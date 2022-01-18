8f7ac73ab9c81c0d5caadd228dcbc71e4cb834b1
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-17 23:13:11 -0600

    add pretty user menu

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,6 @@
+import { Popover, Portal, Transition } from "@headlessui/react"
+import { Fragment, useState } from "react"
+import { usePopper } from "react-popper"
 import type { LinksFunction, LoaderFunction, MetaFunction } from "remix"
 import {
   Form,
@@ -26,12 +29,14 @@ export const links: LinksFunction = () => [
 ]
 
 type LoaderData = {
-  user?: {
-    name: string
-    avatarUrl?: string
-  }
+  user?: LoaderDataUser
   anilistClientId: string
   anilistRedirectUri: string
+}
+
+type LoaderDataUser = {
+  name: string
+  avatarUrl?: string
 }
 
 export const loader: LoaderFunction = async ({
@@ -39,13 +44,15 @@ export const loader: LoaderFunction = async ({
 }): Promise<LoaderData> => {
   const session = await getSession(request)
 
-  let user: LoaderData["user"] | undefined
+  let user: LoaderDataUser | undefined
   if (session) {
     const client = createAuthenticatedClient(session.accessToken)
-    const result = await client.query({
-      query: ViewerDocument,
-    })
-    if (result.data.Viewer) {
+
+    const result = await client
+      .query({ query: ViewerDocument })
+      .catch(() => undefined)
+
+    if (result?.data.Viewer) {
       user = {
         name: result.data.Viewer.name,
         avatarUrl: result.data.Viewer.avatar?.medium,
@@ -63,14 +70,6 @@ export const loader: LoaderFunction = async ({
 }
 
 export default function App() {
-  const data = useLoaderData<LoaderData>()
-
-  const loginUrl =
-    `https://anilist.co/api/v2/oauth/authorize` +
-    `?client_id=${data.anilistClientId}` +
-    `&redirect_uri=${data.anilistRedirectUri}` +
-    `&response_type=code`
-
   return (
     <html lang="en" className="bg-slate-900 text-slate-100">
       <head>
@@ -80,51 +79,115 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <header className="shadow bg-slate-800">
-          <div className={maxWidthContainerClass}>
-            <div className="flex items-center justify-between h-16">
-              <a href="/" title="Home" className="translate-y-[-2px]">
-                <h1 className="text-3xl font-light">
-                  <span className="text-sky-400">ani</span>
-                  <span className="text-emerald-400">green</span>
-                </h1>
-              </a>
-
-              {data.user ? (
-                <Form action="/logout" method="post">
-                  <button
-                    type="submit"
-                    className="flex items-center gap-2 transition opacity-50 hover:opacity-100 focus:opacity-100"
-                  >
-                    <img
-                      src={data.user.avatarUrl}
-                      alt={`Logged in as ${data.user.name}`}
-                      className="w-8 h-8 rounded-full"
-                    />
-                  </button>
-                </Form>
-              ) : (
-                <a
-                  href={loginUrl}
-                  className={buttonClass({ variant: "clear" })}
-                >
-                  log in with AniList
-                </a>
-              )}
-            </div>
-          </div>
-        </header>
-
+        <AppHeader />
         <div className="h-4" />
-
         <main className={maxWidthContainerClass}>
           <Outlet />
         </main>
-
         <ScrollRestoration />
         <Scripts />
         {process.env.NODE_ENV === "development" && <LiveReload />}
       </body>
     </html>
+  )
+}
+
+function AppHeader() {
+  const data = useLoaderData<LoaderData>()
+
+  const loginUrl =
+    `https://anilist.co/api/v2/oauth/authorize` +
+    `?client_id=${data.anilistClientId}` +
+    `&redirect_uri=${data.anilistRedirectUri}` +
+    `&response_type=code`
+
+  return (
+    <header className="shadow bg-slate-800">
+      <div className={maxWidthContainerClass}>
+        <div className="flex items-center justify-between h-16">
+          <a href="/" title="Home" className="translate-y-[-2px]">
+            <h1 className="text-3xl font-light">
+              <span className="text-sky-400">ani</span>
+              <span className="text-emerald-400">green</span>
+            </h1>
+          </a>
+          {data.user ? (
+            <UserMenuButton user={data.user} />
+          ) : (
+            <a href={loginUrl} className={buttonClass({ variant: "clear" })}>
+              log in with AniList
+            </a>
+          )}
+        </div>
+      </div>
+    </header>
+  )
+}
+
+function UserMenuButton({ user }: { user: LoaderDataUser }) {
+  const [referenceElement, setReferenceElement] = useState<HTMLElement | null>()
+  const [popperElement, setPopperElement] = useState<HTMLElement | null>()
+
+  const { styles, attributes, update } = usePopper(
+    referenceElement,
+    popperElement,
+    {
+      placement: "bottom-end",
+      modifiers: [{ name: "offset", options: { offset: [0, 8] } }],
+    },
+  )
+
+  return (
+    <Popover>
+      <div className="relative">
+        <Popover.Button
+          className="transition opacity-50 hover:opacity-100 focus:opacity-100"
+          ref={setReferenceElement}
+        >
+          <img
+            src={user.avatarUrl}
+            alt={`Logged in as ${user.name}`}
+            className="w-8 h-8 rounded-full"
+          />
+        </Popover.Button>
+        <Portal>
+          <div
+            ref={setPopperElement}
+            style={styles.popper}
+            {...attributes.popper}
+          >
+            <Transition
+              as={Fragment}
+              enter="transition"
+              enterFrom="transform translate-y-3 opacity-0"
+              enterTo="transform translate-y-0 opacity-100"
+              leave="transition"
+              leaveFrom="transform translate-y-0 opacity-100"
+              leaveTo="transform translate-y-3 opacity-0"
+            >
+              <Popover.Panel className="absolute right-0 flex flex-col overflow-hidden font-medium rounded-lg shadow top-full bg-slate-50 text-slate-900 w-max">
+                <p className="px-3 py-1.5 opacity-60">
+                  Logged in as {user.name}
+                </p>
+                <div className="h-px bg-slate-300" />
+                <Form
+                  action="/logout"
+                  method="post"
+                  replace
+                  className="contents"
+                >
+                  <button
+                    type="submit"
+                    className="px-3 py-1.5 transition hover:bg-emerald-300 focus:bg-emerald-300 text-left"
+                  >
+                    Log out
+                  </button>
+                </Form>
+              </Popover.Panel>
+            </Transition>
+          </div>
+        </Portal>
+      </div>
+    </Popover>
   )
 }
