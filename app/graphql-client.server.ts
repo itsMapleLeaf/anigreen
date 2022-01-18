99265240c056bdecf13df0668e6e436cf5e9f66e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-18 00:06:06 -0600

    key caches by access token or global

--- app/graphql-client.server.ts
+++ app/graphql-client.server.ts
@@ -1,18 +1,28 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
-const cache = new InMemoryCache()
+const caches = new Map<string | symbol, InMemoryCache>()
+const globalCacheKey = Symbol("globalCacheKey")
+
+function getCache(key: string | symbol): InMemoryCache {
+  let cache = caches.get(key)
+  if (!cache) {
+    cache = new InMemoryCache()
+    caches.set(key, cache)
+  }
+  return cache
+}
 
 export function createClient() {
   return new ApolloClient({
     uri: "https://graphql.anilist.co/",
-    cache,
+    cache: getCache(globalCacheKey),
   })
 }
 
 export function createAuthenticatedClient(accessToken: string) {
   return new ApolloClient({
     uri: "https://graphql.anilist.co/",
-    cache,
+    cache: getCache(accessToken),
     headers: {
       authorization: `Bearer ${accessToken}`,
     },
