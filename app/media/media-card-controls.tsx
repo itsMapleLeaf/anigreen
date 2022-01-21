e7a61dec57bd3cd036c668d985a8528f37432e89
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-20 22:55:02 -0600

    inline auth wrapper

--- app/media/media-card-controls.tsx
+++ app/media/media-card-controls.tsx
@@ -1,5 +1,4 @@
 import { BookmarkIcon } from "@heroicons/react/solid"
-import type { ReactNode } from "react"
 import { useAuthContext } from "~/auth/auth-context"
 import type { MediaResource } from "~/media/media"
 import { actionButtonClass } from "~/media/media-card-action-button"
@@ -22,11 +21,14 @@ export function MediaCardControls({ media }: { media: MediaResource }) {
     <div className="bg-slate-800 grid grid-flow-col auto-cols-fr">
       {state.status === "loggedOut" && (
         <>
-          <AuthRequiredWrapper tooltipText="Log in to bookmark this">
-            <div className={actionButtonClass}>
-              <BookmarkIcon className="w-5" />
+          <Tooltip text="Log in to bookmark this" side="bottom">
+            {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
+            <div tabIndex={0} className="opacity-40">
+              <div className={actionButtonClass}>
+                <BookmarkIcon className="w-5" />
+              </div>
             </div>
-          </AuthRequiredWrapper>
+          </Tooltip>
           <MediaCardLinksButton media={media} />
         </>
       )}
@@ -49,22 +51,5 @@ export function MediaCardControls({ media }: { media: MediaResource }) {
         </>
       )}
     </div>
-  )
-}
-
-function AuthRequiredWrapper({
-  tooltipText,
-  children,
-}: {
-  tooltipText: ReactNode
-  children: ReactNode
-}) {
-  return (
-    <Tooltip text={tooltipText} side="bottom">
-      {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
-      <div tabIndex={0}>
-        <div className="pointer-events-none opacity-40">{children}</div>
-      </div>
-    </Tooltip>
   )
 }
