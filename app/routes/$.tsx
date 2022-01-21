208b2243de7f867c81368251bc8abb28b1b40a38
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-20 23:24:25 -0600

    add simple 404 page

---
+++ app/routes/$.tsx
@@ -0,0 +1,14 @@
+import { getAppTitle } from "~/meta"
+
+export const meta = () => ({
+  title: getAppTitle("Page not found"),
+})
+
+export default function NotFound() {
+  return (
+    <>
+      <h1 className="text-4xl font-light mb-4">oops</h1>
+      <p>{`couldn't find that page :(`}</p>
+    </>
+  )
+}
