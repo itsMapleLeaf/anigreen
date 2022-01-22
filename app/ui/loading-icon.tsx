2ba5a1e244b0f9265ca6206da495f71df8ce9556
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-22 15:57:07 -0600

    add icon size prop

--- app/ui/loading-icon.tsx
+++ app/ui/loading-icon.tsx
@@ -1,6 +1,16 @@
-export function LoadingIcon() {
+import clsx from "clsx"
+
+type LoadingIconSize = "normal" | "large"
+
+export function LoadingIcon({ size = "normal" }: { size?: LoadingIconSize }) {
   return (
-    <div className="grid grid-cols-2 gap-1 p-[1px] w-5 h-5 animate-spin">
+    <div
+      className={clsx(
+        "grid grid-cols-2 animate-spin",
+        size === "normal" && `gap-1 p-[1px] w-5 h-5`,
+        size === "large" && `gap-1.5 w-12 h-12`,
+      )}
+    >
       <div className="bg-emerald-500 rounded-full" />
       <div className="bg-emerald-100 rounded-full" />
       <div className="bg-emerald-100 rounded-full" />
