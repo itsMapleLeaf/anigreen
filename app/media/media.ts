75b4090848523fbf41f7591f92ed784c925d0bcb
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-22 16:52:58 -0600

    helper for creating a media resource

--- app/media/media.ts
+++ app/media/media.ts
@@ -1,4 +1,10 @@
-import type { MediaExternalLink, MediaListStatus } from "~/anilist/graphql.out"
+import type {
+  MediaExternalLink,
+  MediaFragment,
+  MediaListEntryFragment,
+  MediaListStatus,
+} from "~/anilist/graphql.out"
+import { isTruthy } from "~/helpers/is-truthy"
 
 export type MediaResource = {
   id: number
@@ -9,12 +15,43 @@ export type MediaResource = {
   coverColor?: string
   episodeCount?: number
   anilistUrl?: string
-  watchListInfo?: MediaWatchListInfo
   externalLinks: MediaExternalLink[]
+  watchListInfo?: MediaWatchListInfo
 }
 
 export type MediaWatchListInfo = {
   mediaListId: number
   status: MediaListStatus
   progress: number
+}
+
+export function createMediaResource(
+  mediaData: MediaFragment,
+  mediaListData: MediaListEntryFragment | undefined,
+): MediaResource {
+  const { title } = mediaData ?? {}
+
+  const titleText =
+    title?.userPreferred ||
+    title?.english ||
+    title?.romaji ||
+    title?.native ||
+    "Unknown Title"
+
+  return {
+    id: mediaData.id,
+    title: titleText,
+    format: mediaData.format?.replace(/[^A-Za-z]+/g, " "),
+    episodeCount: mediaData.episodes,
+    bannerImageUrl: mediaData.bannerImage,
+    coverImageUrl: mediaData.coverImage?.medium,
+    coverColor: mediaData.coverImage?.color,
+    anilistUrl: mediaData.siteUrl,
+    externalLinks: mediaData.externalLinks?.filter(isTruthy) ?? [],
+    watchListInfo: mediaListData?.status && {
+      mediaListId: mediaListData.id,
+      status: mediaListData.status,
+      progress: mediaListData.progress ?? 0,
+    },
+  }
 }
--- app/schedule/schedule-data.server.ts
+++ app/schedule/schedule-data.server.ts
@@ -2,9 +2,9 @@ import { startOfDay } from "date-fns"
 import { anilistClient } from "~/anilist/anilist-client.server"
 import { ScheduleDocument } from "~/anilist/graphql.out"
 import { startOfDayZoned } from "~/dates/start-of-day-zoned"
-import { isTruthy } from "~/helpers/is-truthy"
 import { mapGetWithFallback } from "~/helpers/map-get-with-fallback"
 import type { MediaResource } from "~/media/media"
+import { createMediaResource } from "~/media/media"
 
 export type ScheduleData = Awaited<ReturnType<typeof loadScheduleData>>
 
@@ -33,34 +33,11 @@ export async function loadScheduleData(
     if (!schedule || !schedule.media) continue
 
     const day = startOfDay(schedule.airingAt * 1000).getTime()
-    const { title } = schedule.media ?? {}
-
-    const titleText =
-      title?.userPreferred ||
-      title?.english ||
-      title?.romaji ||
-      title?.native ||
-      "Unknown Title"
 
     mapGetWithFallback(itemsByDay, day, []).push({
       id: schedule.id,
       episode: schedule.episode,
-      media: {
-        id: schedule.media.id,
-        title: titleText,
-        format: schedule.media.format?.replace(/[^A-Za-z]+/g, " "),
-        episodeCount: schedule.media.episodes,
-        bannerImageUrl: schedule.media.bannerImage,
-        coverImageUrl: schedule.media.coverImage?.medium,
-        coverColor: schedule.media.coverImage?.color,
-        anilistUrl: schedule.media.siteUrl,
-        watchListInfo: schedule.media.mediaListEntry?.status && {
-          mediaListId: schedule.media.mediaListEntry.id,
-          status: schedule.media.mediaListEntry.status,
-          progress: schedule.media.mediaListEntry.progress ?? 0,
-        },
-        externalLinks: schedule.media.externalLinks?.filter(isTruthy) ?? [],
-      },
+      media: createMediaResource(schedule.media, schedule.media.mediaListEntry),
     })
   }
 
