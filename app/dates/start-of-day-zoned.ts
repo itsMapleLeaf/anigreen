b05d1a79fa80c1f2a3b46c6692a963a0869093b4
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-22 18:03:08 -0600

    startOfDayZoned: accept number for date

--- app/dates/start-of-day-zoned.ts
+++ app/dates/start-of-day-zoned.ts
@@ -1,7 +1,7 @@
 import { addMilliseconds, subMilliseconds } from "date-fns"
 import { getTimezoneOffset } from "date-fns-tz"
 
-export function startOfDayZoned(date: Date, timezone: string) {
+export function startOfDayZoned(date: number | Date, timezone: string) {
   const timezoneOffset = getTimezoneOffset(timezone)
   const dateWithoutZone = addMilliseconds(date, timezoneOffset)
   const dateWithoutTime = subMilliseconds(
