5c7b34ca08144aef5e14877b0b84774efbf7cf06
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-23 21:07:21 -0600

    add loading placeholder for use later

---
+++ app/modules/ui/loading-placeholder.tsx
@@ -0,0 +1,9 @@
+import { LoadingIcon } from "./loading-icon"
+
+export function LoadingPlaceholder() {
+  return (
+    <div className="grid place-items-center my-4">
+      <LoadingIcon size="large" />
+    </div>
+  )
+}
