a303451c79be23f232b38118699bdad246ebd846
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-23 18:25:22 -0600

    add in progress section to watching page

---
+++ app/helpers/promise-all-object.ts
@@ -0,0 +1,13 @@
+export async function promiseAllObject<
+  Promises extends Record<string, PromiseLike<unknown>>,
+>(
+  promises: Promises,
+): Promise<{ [Key in keyof Promises]: Awaited<Promises[Key]> }> {
+  const result: any = {}
+  await Promise.all(
+    Object.entries(promises).map(async ([key, promise]) => {
+      result[key] = await promise
+    }),
+  )
+  return result
+}
--- app/routes/watching.tsx
+++ app/routes/watching.tsx
@@ -4,9 +4,12 @@ import { anilistClient } from "~/anilist/anilist-client.server"
 import { ViewerDocument, WatchingDocument } from "~/anilist/graphql.out"
 import { getSession } from "~/auth/session.server"
 import { getTimezone } from "~/dates/timezone-cookie.server"
+import { promiseAllObject } from "~/helpers/promise-all-object"
+import type { MediaResource } from "~/media/media"
 import { createMediaResource } from "~/media/media"
 import { MediaCard } from "~/media/media-card"
 import { responseTyped, useLoaderDataTyped } from "~/remix-typed"
+import { GridSection } from "~/ui/grid-section"
 import { WeekdaySectionedList } from "~/ui/weekday-sectioned-list"
 
 export async function loader({ request }: DataFunctionArgs) {
@@ -25,18 +28,19 @@ export async function loader({ request }: DataFunctionArgs) {
     throw responseTyped("", { status: 500, statusText: "user id not found" })
   }
 
-  const data = await anilistClient.request({
-    document: WatchingDocument,
-    variables: { userId },
-    accessToken: session.accessToken,
+  const results = await promiseAllObject({
+    timezone: getTimezone(request),
+    watching: anilistClient.request({
+      document: WatchingDocument,
+      variables: { userId },
+      accessToken: session.accessToken,
+    }),
   })
 
-  const timezone = await getTimezone(request)
-
   return {
-    timezone,
-    mediaItems:
-      data.MediaListCollection?.lists
+    timezone: results.timezone,
+    watchingItems:
+      results.watching.MediaListCollection?.lists
         ?.flatMap((list) => list?.entries ?? [])
         .flatMap((entry) =>
           entry?.media ? createMediaResource(entry.media, entry) : [],
@@ -46,13 +50,34 @@ export async function loader({ request }: DataFunctionArgs) {
 
 export default function WatchingPage() {
   const data = useLoaderDataTyped<typeof loader>()
+
   return (
-    <WeekdaySectionedList
-      items={data.mediaItems}
-      timezone={data.timezone}
-      getItemKey={(media) => media.id}
-      getItemDate={(media) => media.nextAiringEpisode?.airingAtMs}
-      renderItem={(media) => <MediaCard media={media} hideWatchingStatus />}
-    />
+    <>
+      <GridSection title="In progress" subtitle="Catch up on the leftovers">
+        {data.watchingItems.filter(isInProgress).map((media) => (
+          <MediaCard key={media.id} media={media} hideWatchingStatus />
+        ))}
+      </GridSection>
+      <WeekdaySectionedList
+        items={data.watchingItems}
+        timezone={data.timezone}
+        getItemKey={(media) => media.id}
+        getItemDate={(media) => media.nextAiringEpisode?.airingAtMs}
+        renderItem={(media) => <MediaCard media={media} hideWatchingStatus />}
+      />
+    </>
   )
+}
+
+function isInProgress(media: MediaResource) {
+  // if it's endless, it's always in progress (conan pls)
+  if (media.episodeCount == undefined) return true
+
+  const progress = media.watchListInfo?.progress ?? 0
+
+  const currentEpisode = media.nextAiringEpisode
+    ? media.nextAiringEpisode.episode - 1
+    : media.episodeCount
+
+  return progress < currentEpisode
 }
