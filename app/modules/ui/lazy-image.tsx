faae031981c987ed5e938cd87c2a790d0f9278d2
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-29 23:36:24 -0600

    simplify lazy image even more

--- app/modules/ui/lazy-image.tsx
+++ app/modules/ui/lazy-image.tsx
@@ -1,38 +1,30 @@
-import clsx from "clsx"
 import type { ComponentPropsWithoutRef } from "react"
-import { useEffect, useRef } from "react"
-import { raise } from "~/modules/common/errors"
 
 export function LazyImage({
   src,
   alt,
   className,
-  onLoad,
   ...props
 }: ComponentPropsWithoutRef<"img">) {
-  const imageRef = useRef<HTMLImageElement>(null)
-
-  useEffect(() => {
-    const image = imageRef.current ?? raise("image ref not assigned")
-    if (image.complete) return
-
-    image.classList.add("opacity-0")
-    const handleLoad = () => {
-      image.classList.remove("opacity-0")
-      image.classList.add("opacity-100")
-    }
-    image.addEventListener("load", handleLoad)
-    return () => image.removeEventListener("load", handleLoad)
-  }, [])
-
   return (
-    <img
-      {...props}
-      loading="lazy"
-      ref={imageRef}
-      src={src}
-      alt={alt}
-      className={clsx(className, "transition")}
-    />
+    <div className={className}>
+      <img
+        {...props}
+        loading="lazy"
+        ref={(image) => {
+          if (!image) return
+          if (image.complete) return
+
+          image.style.opacity = "0"
+          const handleLoad = () => {
+            image.style.opacity = "1"
+          }
+          image.addEventListener("load", handleLoad, { once: true })
+        }}
+        className="transition-opacity w-full h-full"
+        src={src}
+        alt={alt}
+      />
+    </div>
   )
 }
