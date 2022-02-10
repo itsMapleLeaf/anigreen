2e7f2617d334e665e3505fde74a3f5678577197c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-02-09 22:02:24 -0600

    redirect from index to watching when logged in

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,10 @@
 import type { LoaderFunction } from "remix"
 import { redirect } from "remix"
+import { loadViewerUser } from "~/modules/anilist/user"
+import { getSession } from "~/modules/auth/session.server"
 
-export const loader: LoaderFunction = () => redirect("/schedule", 303)
+export const loader: LoaderFunction = async ({ request }) => {
+  const session = await getSession(request)
+  const user = session && (await loadViewerUser(session.accessToken))
+  return redirect(user ? "/watching" : "/schedule", 303)
+}
