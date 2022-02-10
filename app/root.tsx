7fa89158de525ea3864820974d453869867d75b3
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-02-09 22:00:04 -0600

    add web manifest link

--- app/root.tsx
+++ app/root.tsx
@@ -68,6 +68,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="manifest" href="/manifest.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
