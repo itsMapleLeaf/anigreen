06272c9fe7e2fc56eca085b8bc71e4b23b13ab18
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-02-09 21:58:14 -0600

    working status buttons

--- app/modules/media/media-card-edit-button.tsx
+++ app/modules/media/media-card-edit-button.tsx
@@ -1,6 +1,16 @@
-import { PencilAltIcon } from "@heroicons/react/solid"
+import {
+  PauseIcon,
+  PencilAltIcon,
+  PlayIcon,
+  StopIcon,
+} from "@heroicons/react/solid"
 import { useFetcher } from "remix"
+import { MediaListStatus } from "~/generated/anilist-graphql"
 import { MediaCardActionButton } from "~/modules/media/media-card-action-button"
+import { UpdateMediaListEntryForm } from "~/routes/update-media-list-entry"
+import { Button } from "../ui/button"
+import { activeClearButtonClass, clearButtonClass } from "../ui/button-style"
+import { Menu } from "../ui/menu"
 import { Popover } from "../ui/popover"
 import type { AnilistMedia, AnilistMediaListEntry } from "./media-data"
 
@@ -13,35 +23,60 @@ export function MediaCardEditButton({
 }) {
   const fetcher = useFetcher()
 
+  const currentStatus =
+    fetcher.submission?.formData.get("status") ?? watchListInfo.status
+
   const button = (
     <MediaCardActionButton tooltip="Edit" loading={!!fetcher.submission}>
       <PencilAltIcon className="w-5" />
     </MediaCardActionButton>
   )
 
+  const statusItems = [
+    {
+      status: MediaListStatus.Current,
+      text: "Watching",
+      icon: <PlayIcon className={Menu.leftIconClass} />,
+    },
+    {
+      status: MediaListStatus.Paused,
+      text: "Hold",
+      icon: <PauseIcon className={Menu.leftIconClass} />,
+    },
+    {
+      status: MediaListStatus.Dropped,
+      text: "Drop",
+      icon: <StopIcon className={Menu.leftIconClass} />,
+    },
+  ] as const
+
   return (
     <Popover trigger={button}>
-      <div className="bg-slate-800 shadow rounded-lg p-4">items here</div>
+      <div className="bg-slate-800 shadow rounded-lg p-4">
+        <div className="flex gap-8 px-1.5">
+          {statusItems.map((item) => (
+            <UpdateMediaListEntryForm
+              key={item.status}
+              as={fetcher.Form}
+              data={{ mediaId: media.id, status: item.status }}
+            >
+              <Button
+                type="submit"
+                className={
+                  item.status === currentStatus
+                    ? activeClearButtonClass
+                    : clearButtonClass
+                }
+              >
+                {item.icon}
+                {item.text}
+              </Button>
+            </UpdateMediaListEntryForm>
+          ))}
+        </div>
+      </div>
     </Popover>
   )
-
-  // const statusItems = [
-  //   {
-  //     status: MediaListStatus.Current,
-  //     text: "Watching",
-  //     icon: <PlayIcon className={Menu.leftIconClass} />,
-  //   },
-  //   {
-  //     status: MediaListStatus.Paused,
-  //     text: "Hold",
-  //     icon: <PauseIcon className={Menu.leftIconClass} />,
-  //   },
-  //   {
-  //     status: MediaListStatus.Dropped,
-  //     text: "Drop",
-  //     icon: <StopIcon className={Menu.leftIconClass} />,
-  //   },
-  // ] as const
 
   // return (
   //   <Menu
