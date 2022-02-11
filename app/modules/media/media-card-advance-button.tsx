67f18a085811f63e1c7d035e11401faf5fed9c7a
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-02-10 23:40:13 -0600

    show modal when progressing on last episode

--- app/modules/media/media-card-advance-button.tsx
+++ app/modules/media/media-card-advance-button.tsx
@@ -3,6 +3,7 @@ import { useFetcher } from "remix"
 import { MediaCardActionButton } from "~/modules/media/media-card-action-button"
 import { UpdateMediaListEntryForm } from "~/routes/update-media-list-entry"
 import type { AnilistMedia, AnilistMediaListEntry } from "./media-data"
+import { MediaEditModal } from "./media-edit-modal"
 
 export function MediaCardAdvanceButton({
   media,
@@ -13,7 +14,34 @@ export function MediaCardAdvanceButton({
 }) {
   const fetcher = useFetcher()
 
-  const endingEpisode = media.currentEpisode || media.episodeCount
+  const button = (
+    <MediaCardActionButton
+      tooltip="Advance progress"
+      type="submit"
+      loading={!!fetcher.submission}
+    >
+      <ChevronDoubleRightIcon className="w-5" />
+    </MediaCardActionButton>
+  )
+
+  // if we're about to finish this series,
+  // show a modal for rating, starting with progress on the ending episode
+  // on submit, it should mark the anime completed in anilist
+  if (mediaListEntry.progress + 1 === media.episodeCount) {
+    return (
+      <MediaEditModal
+        media={media}
+        watchListInfo={{
+          ...mediaListEntry,
+          progress: media.episodeCount,
+        }}
+      >
+        {button}
+      </MediaEditModal>
+    )
+  }
+
+  const endingEpisode = media.currentEpisode ?? media.episodeCount
 
   const isInProgress =
     endingEpisode == undefined || mediaListEntry.progress < endingEpisode
@@ -27,13 +55,7 @@ export function MediaCardAdvanceButton({
       as={fetcher.Form}
       data={{ mediaId: media.id, progress: mediaListEntry.progress + 1 }}
     >
-      <MediaCardActionButton
-        tooltip="Advance progress"
-        type="submit"
-        loading={!!fetcher.submission}
-      >
-        <ChevronDoubleRightIcon className="w-5" />
-      </MediaCardActionButton>
+      {button}
     </UpdateMediaListEntryForm>
   )
 }
