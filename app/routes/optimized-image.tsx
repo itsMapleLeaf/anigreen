6418995f664c30d6e8995e2e6c7495df79d3323d
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-07-06 13:29:09 -0500

    cache images with redis

--- app/routes/optimized-image.tsx
+++ app/routes/optimized-image.tsx
@@ -3,7 +3,7 @@ import { redirect } from "@remix-run/node"
 import { z } from "zod"
 import { hash } from "~/modules/common/hash.server"
 import { parseUnsignedInteger } from "~/modules/common/parse-unsigned-integer"
-import { getStorageFile } from "~/modules/gcp/get-storage-file.server"
+import { getRedisClient } from "~/modules/redis"
 import { resizeImage } from "~/modules/resize-image.server"
 
 export async function loader({ request }: DataFunctionArgs) {
@@ -19,11 +19,18 @@ export async function loader({ request }: DataFunctionArgs) {
 
   const imageUrlHash = hash(params.imageUrl)
   const imageFile = `${imageUrlHash}-${params.width}x${params.height}.webp`
+  const cacheKey = `optimized-image:${imageFile}`
 
-  const storageFile = getStorageFile(imageFile)
-  const [storageFileExists] = await storageFile.exists()
-  if (storageFileExists) {
-    return redirect(storageFile.publicUrl(), {
+  const redis = await getRedisClient().catch((error) => {
+    console.warn("Failed to get redis client:", error)
+  })
+
+  const cachedImage = await redis?.get(cacheKey).catch((error) => {
+    console.warn("Failed to get cached image:", error)
+  })
+
+  if (cachedImage) {
+    return redirect(cachedImage, {
       headers: {
         "Content-Type": "image/webp",
         "Cache-Control": "public, max-age=5184000",
@@ -31,15 +38,35 @@ export async function loader({ request }: DataFunctionArgs) {
     })
   }
 
-  const image = await fetch(params.imageUrl).then((res) => res.arrayBuffer())
+  const image = await fetch(params.imageUrl)
+    .then((res) => res.arrayBuffer())
+    .catch((error) => console.warn("Failed to fetch image:", error))
+
+  if (!image) {
+    return new Response(undefined, { status: 500 })
+  }
 
   const resized = await resizeImage(
     Buffer.from(image),
     params.width,
     params.height,
-  )
+  ).catch((error) => console.warn("Failed to resize image:", error))
 
-  storageFile.save(resized).catch(console.error)
+  if (!resized) {
+    // if we couldn't resize the image, send the original image as a fallback
+    return new Response(image, {
+      status: 200,
+      headers: {
+        "Content-Type": "image/png",
+        "Cache-Control": "no-store",
+      },
+    })
+  }
+
+  // failing to cache isn't fatal, but we still want to know if it happens
+  await redis
+    ?.set(cacheKey, resized, { PX: 1000 * 60 * 60 * 24 * 7 })
+    .catch((error) => console.warn("Failed to cache image:", error))
 
   return new Response(resized.buffer, {
     status: 200,
