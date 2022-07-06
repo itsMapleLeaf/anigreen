03334f261825173b988859c4c419cd0f4c30a326
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-07-06 14:03:11 -0500

    remove gcp and vercel stuff

--- app/modules/gcp/get-storage-file.server.ts
+++
@@ -1,6 +0,0 @@
-import { Storage } from "@google-cloud/storage"
-
-export function getStorageFile(path: string) {
-  const storage = new Storage()
-  return storage.bucket("anigreen-resized-images").file(path)
-}
