e215175d5329c6497750d3d5f6e6c8a5d83eefbe
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-04-16 15:47:33 -0500

    simpler signature for infix

--- app/modules/common/infix.ts
+++ app/modules/common/infix.ts
@@ -1,10 +1,10 @@
 /**
  * Returns a new array with a separator in between each item
  */
-export function infix<Separator, Item>(
+export function infix<Item>(
   items: Item[],
-  separator: (index: number) => Separator,
-): Array<Item | Separator> {
+  separator: (index: number) => Item,
+): Item[] {
   return [
     ...items.slice(0, 1),
     ...items.slice(1).flatMap((item, index) => [separator(index - 1), item]),
