8b55f0a5f8ecf90d0704b6aa961a7cf187c7ec91
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-04-16 23:26:17 -0500

    cuter heart

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,9 +1,15 @@
+import { Heart } from "lucide-react"
 import { ExternalLink } from "~/components/external-link"
 
 export function Footer() {
   return (
     <footer className="container text-sm opacity-75">
-      made with ♥️ by{" "}
+      made with{" "}
+      <Heart
+        aria-label="love"
+        className="inline align-text-top text-emerald-300 s-4"
+      />{" "}
+      by{" "}
       <ExternalLink href="https://mapleleaf.dev" className="link">
         MapleLeaf
       </ExternalLink>{" "}
